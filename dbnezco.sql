df13b0b5ea29351bcd4bf81a636b0db2d5d3ce15
yoelismendoza <yoelis.mendoza@laplanamartinez.com>
2018-03-11 21:34:14 -0400

    sql bd

---
+++ dbnezco.sql
@@ -0,0 +1,412 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 12-03-2018 a las 02:32:58
+-- Versión del servidor: 10.1.28-MariaDB
+-- Versión de PHP: 7.0.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `dbnezco`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categoria_imagen`
+--
+
+CREATE TABLE `categoria_imagen` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `descripcion` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `estatus` enum('Activo','Desactivado') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `categoria_imagen`
+--
+
+INSERT INTO `categoria_imagen` (`id`, `descripcion`, `estatus`, `usuario_id`, `created_at`, `updated_at`) VALUES
+(4, 'Producto', 'Activo', 2, '2018-03-11 04:00:00', '2018-03-11 04:00:00'),
+(5, 'Servicio', 'Activo', 2, '2018-03-11 04:00:00', '2018-03-11 04:00:00'),
+(6, 'Galeria', 'Activo', 2, '2018-03-11 04:00:00', '2018-03-11 04:00:00'),
+(7, 'Empresa', 'Activo', 2, '2018-03-11 04:00:00', '2018-03-11 04:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categoria_producto`
+--
+
+CREATE TABLE `categoria_producto` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `descripcion` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `estatus` enum('Activo','Desactivado') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `categoria_producto`
+--
+
+INSERT INTO `categoria_producto` (`id`, `descripcion`, `estatus`, `usuario_id`, `created_at`, `updated_at`) VALUES
+(1, 'Extintores', 'Activo', 2, '2018-03-11 04:00:00', '2018-03-11 04:00:00'),
+(2, 'Mangueras', 'Activo', 2, '2018-03-11 04:00:00', '2018-03-11 04:00:00'),
+(3, 'Polvos Químicos', 'Activo', 2, '2018-03-11 04:00:00', '2018-03-11 04:00:00'),
+(4, 'Pitón', 'Activo', 2, '2018-03-11 04:00:00', '2018-03-11 04:00:00'),
+(5, 'Acople', 'Activo', 2, '2018-03-11 04:00:00', '2018-03-11 04:00:00'),
+(6, 'Siamesa', 'Activo', 2, '2018-03-11 04:00:00', '2018-03-11 04:00:00'),
+(7, 'Devanadora', 'Activo', 2, '2018-03-11 04:00:00', '2018-03-11 04:00:00'),
+(8, 'Niple', 'Activo', 2, '2018-03-11 04:00:00', '2018-03-11 04:00:00'),
+(9, 'Válvula', 'Activo', 2, '2018-03-11 04:00:00', '2018-03-11 04:00:00'),
+(10, 'Bateria', 'Activo', 2, '2018-03-11 04:00:00', '2018-03-11 04:00:00'),
+(11, 'Lámpara', 'Activo', 2, '2018-03-11 04:00:00', '2018-03-11 04:00:00'),
+(12, 'Caja Metálica', 'Activo', 2, '2018-03-11 04:00:00', '2018-03-11 04:00:00'),
+(13, 'Frente de Lámpara', 'Activo', 2, '2018-03-11 04:00:00', '2018-03-11 04:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categoria_servicio`
+--
+
+CREATE TABLE `categoria_servicio` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `descripcion` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `estatus` enum('Activo','Desactivado') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `categoria_servicio`
+--
+
+INSERT INTO `categoria_servicio` (`id`, `descripcion`, `estatus`, `usuario_id`, `created_at`, `updated_at`) VALUES
+(1, 'Prueba 1', 'Activo', 2, '2018-03-12 04:00:00', '2018-03-12 04:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categoria_video`
+--
+
+CREATE TABLE `categoria_video` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `descripcion` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `estatus` enum('Activo','Desactivado') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `categoria_video`
+--
+
+INSERT INTO `categoria_video` (`id`, `descripcion`, `estatus`, `usuario_id`, `created_at`, `updated_at`) VALUES
+(1, 'Principal', 'Activo', 2, '2018-03-12 04:00:00', '2018-03-12 04:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `galeria`
+--
+
+CREATE TABLE `galeria` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nombre` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `publico` enum('Si','No') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `imagen`
+--
+
+CREATE TABLE `imagen` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nombre` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `url` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `categoria_imagen_id` int(11) NOT NULL,
+  `tipo_id` int(11) NOT NULL,
+  `publico` enum('Si','No') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(14, '2014_10_12_000000_create_users_table', 1),
+(15, '2014_10_12_100000_create_password_resets_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `codigo` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nombre` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `descripcion` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `categoria_producto_id` int(11) NOT NULL,
+  `publico` enum('Si','No') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `servicio`
+--
+
+CREATE TABLE `servicio` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `codigo` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nombre` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `descripcion` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `categoria_servicio_id` int(11) NOT NULL,
+  `publico` enum('Si','No') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(2, 'Administrador', 'yoe318@gmail.com', '$2y$10$r7kzW6T6xi25ozsk1tunSegG1R9anftelKYiOy7fzMYSfWTnknu4S', 'aSEvYGYZWMhIfF73SXuRBDWdSr5UBpdTEITRcYIzSHmfDAGzsVHkLdbg6WKo', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `video`
+--
+
+CREATE TABLE `video` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nombre` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `url` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `categoria_video_id` int(11) NOT NULL,
+  `publico` enum('Si','No') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `categoria_imagen`
+--
+ALTER TABLE `categoria_imagen`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `categoria_producto`
+--
+ALTER TABLE `categoria_producto`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `categoria_servicio`
+--
+ALTER TABLE `categoria_servicio`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `categoria_video`
+--
+ALTER TABLE `categoria_video`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `galeria`
+--
+ALTER TABLE `galeria`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `imagen`
+--
+ALTER TABLE `imagen`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `categoria_imagen_id_fk` (`categoria_imagen_id`);
+
+--
+-- Indices de la tabla `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `categoria_producto_id_fk` (`categoria_producto_id`);
+
+--
+-- Indices de la tabla `servicio`
+--
+ALTER TABLE `servicio`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `categoria_servicio_id_fk` (`categoria_servicio_id`);
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `video`
+--
+ALTER TABLE `video`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `categoria_video_id_fk` (`categoria_video_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `categoria_imagen`
+--
+ALTER TABLE `categoria_imagen`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT de la tabla `categoria_producto`
+--
+ALTER TABLE `categoria_producto`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT de la tabla `categoria_servicio`
+--
+ALTER TABLE `categoria_servicio`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `categoria_video`
+--
+ALTER TABLE `categoria_video`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `galeria`
+--
+ALTER TABLE `galeria`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `imagen`
+--
+ALTER TABLE `imagen`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `servicio`
+--
+ALTER TABLE `servicio`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `video`
+--
+ALTER TABLE `video`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
