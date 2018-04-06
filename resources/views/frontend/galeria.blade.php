c47116d92c92eecc878a0c8c7c53925d500ed6ca
Yosecc <yosec.cervino@gmail.com>
2018-04-06 01:56:13 -0400

    FrontV.4

--- resources/views/frontend/galeria.blade.php
+++ resources/views/frontend/galeria.blade.php
@@ -17,17 +17,23 @@
 				{{ $imagenes->url }}
 			@endwhile --}}
 			<div class=" col-12 col-lg-4 col-md-6 py-3">
+
 				<a class="item-g" href="galeria_detail/3/{{ $galeria->id }}">
-					<div class="h-15em overflow-hidden bg-img" style="background-image: url('{{ asset('img/galeria/'.$imagenes->url ) }}')">
-						<div class="item-galery">
-							<div class="row text-center px-3 pt-5">
-								<div class="col">
-									<h3 class="c-white font-italic ">{{ $galeria->nombre }}</h3>
+					
+						
+							<div class="h-15em overflow-hidden bg-img" style="background-image: url('')">
+								<div class="item-galery">
+									<div class="row text-center px-3 pt-5">
+										<div class="col">
+											<h3 class="c-white font-italic ">{{ $galeria->nombre }}</h3>
+										</div>
+									</div>
 								</div>
 							</div>
-						</div>
-					</div>
+					
+					
 				</a>
+
 			</div>
 			@endforeach
 		</div>
